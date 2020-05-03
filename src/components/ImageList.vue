4d17a8a9e8ab018b15fb56bda6632fe9ca3e1198
Panos <anastapd@gmail.com>
2020-05-03 13:21:05 +0100

    Implemented uploading images and hiding content on logout

--- src/components/ImageList.vue
+++ src/components/ImageList.vue
@@ -1,18 +1,36 @@
 <template>
   <div>
-    <img v-for="image in allImages" :key="image.id" :src="image.link" />
+    <div v-if="isLoggedIn" class="image-container">
+      <img v-for="image in allImages" :key="image.id" :src="image.link" />
+    </div>
+    <h2 v-else>
+      Login to get started
+    </h2>
   </div>
 </template>
 
 <script>
-import { mapActions, mapGetters } from "vuex";
+import { mapActions, mapGetters } from 'vuex';
 
 export default {
-  name: "ImageList",
-  methods: mapActions(["fetchImages"]),
-  computed: mapGetters(["allImages"]),
+  name: 'ImageList',
+  methods: mapActions(['fetchImages']),
+  computed: mapGetters(['allImages', 'isLoggedIn']),
   created() {
     this.fetchImages();
-  }
+  },
 };
 </script>
+
+<style scoped>
+.image-container {
+  column-count: 3;
+  column-gap: 0;
+}
+
+img {
+  max-width: 100%;
+  max-height: 300px;
+  padding: 5px;
+}
+</style>
--- src/store/modules/images.js
+++ src/store/modules/images.js
@@ -1,12 +1,15 @@
 import api from '../../api/imgur';
+import {
+    router
+} from '../../main';
 
 const state = {
-    images: []
-}
+    images: [],
+};
 
 const getters = {
-    allImages: state => state.images
-}
+    allImages: (state) => state.images,
+};
 
 const actions = {
     async fetchImages({
@@ -19,19 +22,29 @@ const actions = {
 
         const response = await api.fetchImages(token);
         commit('setImages', response.data.data);
+    },
+    async uploadImages({
+        rootState
+    }, images) {
+        const {
+            token
+        } = rootState.auth;
 
-    }
-}
+        await api.uploadImages(images, token);
+
+        router.push('/');
+    },
+};
 
 const mutations = {
     setImages: (state, images) => {
         state.images = images;
-    }
-}
+    },
+};
 
 export default {
     state,
     getters,
     actions,
-    mutations
-}
+    mutations,
+};
